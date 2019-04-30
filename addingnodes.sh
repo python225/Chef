8d401057eaa9eaecfd0a501e4fde97635feedd60
python225 <pulabala.rajesh@gmail.com>
2019-04-30 14:14:32 -0500

    Update addingnodes.sh

--- addingnodes.sh
+++ addingnodes.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 
 #knife bootstrap skoruzuser2.mylabserver.com -x root -P fremont555 --node-name 'test-server'
-sudo knife bootstrap 40.86.82.11 --ssh-user ubuntu -i ~/.ssh/id_rsa --sudo --node-name 'usdelprodcelvm.usdelivered.com'
+sudo knife bootstrap x.x.x.x --ssh-user ubuntu -i ~/.ssh/id_rsa --sudo --node-name 'example.com'
 #sudo knife bootstrap 54.196.128.5 --ssh-user root --ssh-password fremont555 --node-name 'testing'
 
 
